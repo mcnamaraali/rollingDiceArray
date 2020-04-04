aed520cfcced0df2548521a634221d33f0a4c358
Ali McNamara <mcnamaraali@gmail.com>
2020-04-04 16:52:45 +0200

    Init commit of rolling dice using an array

---
+++ https:/github.com/mcnamaraali/rollingDiceArray/rollingProj/src/rollingProj/RollingDie.java
@@ -0,0 +1,24 @@
+package rollingProj;
+import java.security.SecureRandom;
+
+public class RollingDie {
+
+	public static void main(String[] args) {
+		SecureRandom randomNumbers = new SecureRandom();
+		int[] frequency = new int[7]; // array of frequency counters
+		
+		//roll die 60,000,000 times, use die value as frequency index
+		for (int roll = 1; roll <= 60_000_000; roll++) {
+			++frequency [1 + randomNumbers.nextInt(6)];
+		}
+		
+		System.out.printf("%s%10s%n", "face", "frequency");
+		
+		//output each array element's value
+		for (int face = 1; face < frequency.length; face++) {
+			System.out.printf("%4d%10d%n", face, frequency[face]);
+		}
+
+	}
+
+}
